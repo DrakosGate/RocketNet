0764c15bf725f65e907c35ba08d6473d5f8c217d
Chris <christopher.howlett@rocketwerkz.com>
2022-08-09 14:51:58 +1200

    Test executable compiling

--- Source/library.cpp
+++ Source/library.cpp
@@ -4,5 +4,6 @@
 #include "RakNet/RakPeerInterface.h"
 
 void hello() {
+    RakNet::RakPeerInterface *peer = RakNet::RakPeerInterface::GetInstance();
     std::cout << "Hello, World!" << std::endl;
 }
---
+++ Source/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by christopher.howlett on 09/08/2022.
+//
+#include "library.h"
+
+int main()
+{
+    hello();
+}
